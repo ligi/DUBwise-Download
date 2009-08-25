3b5b275af3d73ade05ca26f525ff8bf010feb100
marcus bueschleb <ligi@ligi.de>
2009-08-25 18:59:43 +0200

    adding meta-info servlet - this will be used by installer instead of a file in svn

---
+++ src/download/MetaInfoServlet.java
@@ -0,0 +1,39 @@
+package org.ligi.dubwise.download;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.*;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+import org.ligi.dubwise.download.PMF;
+
+public class MetaInfoServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger( EnterCodeServlet.class.getName());
+
+
+ public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws  IOException {
+     String[] uri_path= request.getRequestURI().split("/");
+     String  requested_info =uri_path[uri_path.length-1];
+     if( requested_info.equals("dl_url"))
+	 {
+	     response.getWriter().println("http://ligi.selfip.org/ligi/");
+	 }
+	 else
+	     response.getWriter().println("?");
+	     
+     
+  }
+
+}
