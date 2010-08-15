07cbc649472781e43906f5e695c9fd135f261656
marcus bueschleb <ligi@ligi.de>
2010-08-15 16:00:07 +0200

    improve code style

--- src/org/ligi/dubwise/download/SourceInfoProvider.java
+++ src/org/ligi/dubwise/download/SourceInfoProvider.java
@@ -6,104 +6,85 @@ import javax.cache.CacheManager;
 import java.util.Collections;
 import java.util.Random;
 
-import java.net.*;
-import java.io.*;
-import java.nio.*;
-
-
-
 public class SourceInfoProvider {
 
     public final static String cache_key_installer_version="source_info_installer_version";
   
-
-    public static String act_installer_version()
-    {
-	return "0.19";
+    public static String act_installer_version() {
+    	return "0.19";
     }
 
-    public static String act_version()
-    {
+    public static String act_version() {
 
-	Cache cache = null;
-	try {
-	    cache = CacheManager.getInstance().getCacheFactory().createCache(Collections.emptyMap());
-	} catch (CacheException e) {	}
-
-	if (cache != null) {
-	    try
-		{
-		    if (cache.get(cache_key_installer_version)!=null)
-			return (String) cache.get(cache_key_installer_version);
-		}
-	    catch (Exception e) {}
-	}
-
-	// couldnt get it from the cache - refetch
+		Cache cache = null;
+		try {
+		    cache = CacheManager.getInstance().getCacheFactory().createCache(Collections.emptyMap());
+		} catch (CacheException e) {	}
 	
-	String res;
-	String[] res_a;
-	try
-	    {
-		// had a caching problem with urlfetch - solved with this hint http://osdir.com/ml/GoogleAppEngine/2009-03/msg00125.html
-		Random random=new Random();
-		res_a=InstallHelper.read_http_str(getDownloadURL()+"trunk/version?"+random.nextInt()).replace("\n", "").replace("\r", "") .split("\\.");
-			res=res_a[0]+"."+res_a[1]+res_a[2];
-		cache.put(cache_key_installer_version,res);
-	    }
-	catch (Exception e) { res= "error";}
-	return res;
+		if (cache != null) {
+		    try	{
+			    if (cache.get(cache_key_installer_version)!=null)
+				return (String) cache.get(cache_key_installer_version);
+			}
+		    catch (Exception e) {}
+		}
+	
+		// couldnt get it from the cache - refetch
+		
+		String res;
+		String[] res_a;
+		try  {
+			// had a caching problem with urlfetch - solved with this hint http://osdir.com/ml/GoogleAppEngine/2009-03/msg00125.html
+			Random random=new Random();
+			res_a=InstallHelper.read_http_str(getDownloadURL()+"trunk/version?"+random.nextInt()).replace("\n", "").replace("\r", "") .split("\\.");
+				res=res_a[0]+"."+res_a[1]+res_a[2];
+			cache.put(cache_key_installer_version,res);
+		    }
+		catch (Exception e) { res= "error";}
+		return res;
     }
 
-    public static void update_act_version()
-    {
-	Cache cache = null;
-	try {
-	    cache = CacheManager.getInstance().getCacheFactory().createCache(Collections.emptyMap());
-	    cache.put(cache_key_installer_version, null);
-	} catch (Exception e) {	}
-
+    public static void update_act_version() {
+    	Cache cache = null;
+    	try {
+    		cache = CacheManager.getInstance().getCacheFactory().createCache(Collections.emptyMap());
+    		cache.put(cache_key_installer_version, null);
+    	} catch (Exception e) {	}
     }
 
 
-    // could be used to balance between diffrent servers
-    public static String getDownloadURL()
-    {
-	return "http://ligi.selfip.org/ligi/dubwise_dl/";
+    /**
+     * as a function so that could be used to balance 
+     * between different servers
+     * 
+     * @return the URL where the binaries are
+     */
+    public static String getDownloadURL() {
+    	return "http://ligi.selfip.org/ligi/dubwise_dl/";
     }
     
-    public static String getStableDownloadURL()
-    {
-	return getDownloadURL() + "tags/";
+    public static String getStableDownloadURL() {
+    	return getDownloadURL() + "tags/";
     }
 
-    public static String getBleedingDownloadURL()
-    {
-	return getDownloadURL() + "trunk/" ;
+    public static String getBleedingDownloadURL() {
+    	return getDownloadURL() + "trunk/" ;
     }
     
-    public static String install_notify_URL()
-    {
+    public static String install_notify_URL() {
     	return "http://dubwise-download.appspot.com/notify?install=";
     }
 
-    public static String delete_notify_URL()
-    {
+    public static String delete_notify_URL() {
     	return "http://dubwise-download.appspot.com/notify?delete=";
     }
 
-    public static String installer_extern_base_URL()
-    {
+    public static String installer_extern_base_URL() {
     	return "http://dubwise-download.appspot.com/midlet_download/DUBwiseInstaller";
     }
 
 
-    public static String installer_source_base_URL()
-    {
+    public static String installer_source_base_URL() {
     	return getDownloadURL()+"DUBwiseInstaller";
     }
-
-
-
-    
 }
